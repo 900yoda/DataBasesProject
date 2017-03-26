75671483de4e5890c5167c1c38bac304eed8e932
garnes <garnes@lab2-1.eng.utah.edu>
2017-03-26 12:31:17 -0600

    completed TH

--- Phase2/src/MainFile.java
+++ Phase2/src/MainFile.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+package src;
+
+
+
 //the file is modified
 import java.sql.*;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.*;
+import java.text.*;
 
 
 class Display{
@@ -32,10 +34,12 @@ class Page{
 	protected static Scanner input;
 	protected static PageErrors errors;
 	protected static String user;
+	protected static DateFormat format; 
 	
 	public Page(){
 		input = new Scanner(System.in);
 		errors = new PageErrors();
+		format = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
 	}
 	
 	public void Action(Display display){
@@ -222,8 +226,8 @@ class CreateNewAccountPage extends Page{
 
 class MainPage extends Page{
 	public void Action(Display display){
-		System.out.println("1. Reserve");
-		System.out.println("2. New TH");
+		System.out.println("\n1. Reserve");
+		System.out.println("2. TH Information");
 		System.out.println("3. Stays");
 		System.out.println("4. Favorite TH");
 		System.out.println("5. Feedback");
@@ -315,7 +319,7 @@ class ReservePage extends Page{
 
 class NewTHPage extends Page{
 	public void Action(Display display){
-		System.out.println("1. Add New Home");
+		System.out.println("\n1. Add New Home");
 		System.out.println("2. Update Home");
 		System.out.println("3. Go back");
 		System.out.println("4. Quit");
@@ -443,19 +447,23 @@ class AddNewHomePage extends Page{
 
 class UpdateHomePage extends Page{
 	public void Action(Display display){
-		Set<Integer> ownedHomes = new HashSet<Integer>();
-
+		//Set<Integer> ownedHomes = new HashSet<Integer>();
+		int[] ownedHomes = null;
 		try{
 			Connector conn = new Connector();
 			ResultSet rs=conn.stmt.executeQuery("select th.house_id, th.name, th.address from Temporary_Housing th, Home_Ownership ho where " +
 					"ho.username = '"+ user + "' and ho.house_id = th.house_id");
 			 ResultSetMetaData rsmd = rs.getMetaData();
 	  		 int numCols = rsmd.getColumnCount();
+	  		 ownedHomes = new int[numCols];
+	  		 System.out.println();
+	  		 int index = 0;
 	  		 while (rs.next())
 	  		 {
-	  			 //System.out.print("cname:");
-	  			 ownedHomes.add(rs.getInt(1));
-	  			 for (int i=1; i<=numCols;i++)
+	  			 //System.out.print("cname:"); 
+	  			 ownedHomes[index++] = rs.getInt(1);
+	  			System.out.print(index+ ": ");
+	  			 for (int i=2; i<=numCols;i++)
 	  				 System.out.print(rs.getString(i)+"  ");
 	  			 System.out.println("");
 	  		 }
@@ -474,85 +482,317 @@ class UpdateHomePage extends Page{
 				System.out.print("Please enter number coorisponding to one of your homes id (enter -1 to cancel):  ");
 				choice = input.nextInt();
 				input.nextLine();
+				if(choice == -1)
+					break;
 			}
 			
 			catch(Exception e){
 				errors.IntegerError();
 				input.nextLine();
 			}
-		}while(choice != -1 && !ownedHomes.contains(choice));
+		}while(choice < 1 || choice > ownedHomes.length);
 		
 		if(choice == -1){
 			display.page = new NewTHPage();
 			return;
 		}
 		
-		int home = choice;
-		
-		while(true){
-			try{
-				System.out.println("1. Add Dates");
-				System.out.println("2. Add KeyWords");
-				System.out.println("3. Go Back");
-				System.out.println("4. Quit");
-				choice = input.nextInt();
-				input.nextLine();
-				if(choice >= 1 && choice <= 4)
-					break;
+		int home = ownedHomes[choice-1];
+		boolean go = true;
+		while(go){		
+			while(true){
+				try{
+					System.out.println("\n1. Add Available Dates");
+					System.out.println("2. Remove Available Dates");
+					System.out.println("3. View Available Dates");
+					System.out.println("4. Add KeyWords");
+					System.out.println("5. Remove KeyWords");
+					System.out.println("6. View Keywords");
+					System.out.println("7. View users that have reserved your TH");
+					System.out.println("8. Go Back");
+					System.out.println("9. Quit");
+					System.out.print("Please enter corrisponding number:  ");
+					choice = input.nextInt();
+					input.nextLine();
+					if(choice >= 1 && choice <= 9)
+						break;
+				}
+				catch(Exception e){
+					errors.IntegerError();
+					input.nextLine();
+				}
 			}
-			catch(Exception e){
-				errors.IntegerError();
-				input.nextLine();
-			}
-		}
 		
-		switch(choice){
-		case 1:
-			break;
-		case 2:
-			System.out.print("Please enter in new home keywords with a space between each new keyword:  ");
-			String[] words = input.nextLine().split("[ ]+");
-			String sql = "INSERT INTO Keywords VALUES(?,?)";
-			
-			try{
-				Connector conn = new Connector();
-				ResultSet rs = null;
-				PreparedStatement pstmt = conn.con.prepareStatement(sql);
+
+			switch(choice){
+			case 1:
+				ShowDates(home);
+				System.out.println("\nPlease enter in your to date a hyphen and your from date,"); 
+				System.out.println("with a space in between each new date interval");
+				System.out.print("Example format: mm/dd/yyyy-mm/dd/yyy  mm/dd/yyyy-mm/dd/yyyy ...:  ");
 				
-				for(int i = 0; i < words.length; i++){
-					String name = words[i];
-					rs=conn.stmt.executeQuery("Select keyword from Keywords where Keywords.keyword = '"+name+"' AND Keywords.house_id = "
-					+ Integer.toString(home)+";");
-					if(rs.next())
-						System.out.println(name + "already exists so it was ignored");
-					else{
-						pstmt.clearParameters();
-						pstmt.setInt(1,home);
-						pstmt.setString(2,name);
-						pstmt.executeUpdate();
+				try{
+					String[] dates = input.nextLine().split("[ -]+");
+					String sql = "select d.from_date, d.until_date"
+							+ " from (select from_date, until_date"
+							+ 		" from Dates_Available"
+							+ 		" where house_id = ?) as d"
+							+ " where (STR_TO_DATE(?,'%m/%d/%Y') >= d.from_date and STR_TO_DATE(?,'%m/%d/%Y') <= d.until_date) "
+							+ " or (STR_TO_DATE(?,'%m/%d/%Y') >= d.from_date and STR_TO_DATE(?,'%m/%d/%Y') <= d.until_date) "
+							+ " or (STR_TO_DATE(?,'%m/%d/%Y') <= d.from_date and STR_TO_DATE(?,'%/%d/%Y') >= d.until_date);";
+					
+					String sql2 = "insert into Dates_Available values(?,STR_TO_DATE(?,'%m/%d/%Y'),STR_TO_DATE(?,'%m/%d/%Y'));";
+					
+					Connector conn = new Connector();
+					PreparedStatement pstmt = conn.con.prepareStatement(sql);
+					PreparedStatement pstmt2 = conn.con.prepareStatement(sql2);
+					for(int i = 0; i < dates.length; i+=2){
+						java.util.Date date1 = format.parse(dates[i]);
+						java.util.Date date2 = format.parse(dates[i+1]);
+						
+						if(date1.after(date2))
+							System.out.println(dates[i]+ " is after "+dates[i+1]+" so it will be ignored");
+						else{
+							pstmt.clearParameters();
+							pstmt.setInt(1, home);
+							pstmt.setString(2, dates[i]);
+							pstmt.setString(3, dates[i]);
+							pstmt.setString(4, dates[i+1]);
+							pstmt.setString(5, dates[i+1]);
+							pstmt.setString(6, dates[i]);
+							pstmt.setString(7, dates[i+1]);
+							ResultSet rs = pstmt.executeQuery();
+							if(rs.next())
+								System.out.println(dates[i]+"-"+dates[i+1]+" interferes with "+format.format(rs.getDate(1))+
+										"-"+format.format(rs.getDate(2))+ " so it was not added");
+							else{
+								pstmt2.clearParameters();
+								pstmt2.setInt(1,home);
+								pstmt2.setString(2,dates[i]);
+								pstmt2.setString(3,dates[i+1]);
+								pstmt2.executeUpdate();
+								System.out.println(dates[i] + "-"+ dates[i+1]+ " added");
+							}
+							rs.close();
+							
+						}
 					}
+					conn.closeConnection();
+				}
+				catch(ParseException e){
+					System.out.println("Date entered incorrectly");
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+				break;
+			case 2:
+				ShowDates(home);
+				System.out.println("\nPlease enter in your to date a hyphen and your from date,"); 
+				System.out.println("with a space in between each date interval to be deleted");
+				System.out.print("Example format: mm/dd/yyyy-mm/dd/yyy  mm/dd/yyyy-mm/dd/yyyy ...:  ");
+				try{
+					Connector conn = new Connector();
+					String[] dates = input.nextLine().split("[ -]+");
+					String sql = "delete from Dates_Available where Dates_Available.house_id = ? and"
+							+ " Dates_Available.from_date = STR_TO_DATE(?,'%m/%d/%Y') and Dates_Available.until_date = STR_TO_DATE(?,'%m/%d/%Y');";
+					PreparedStatement pstmt = conn.con.prepareStatement(sql);
+					
+					String sql2 = "select * from Dates_Available where Dates_Available.house_id = ? and"
+							+ " Dates_Available.from_date = STR_TO_DATE(?,'%m/%d/%Y') and Dates_Available.until_date = STR_TO_DATE(?,'%m/%d/%Y');";
+					PreparedStatement pstmt2 = conn.con.prepareStatement(sql2);
+					
+					for(int i = 0; i < dates.length; i+=2){
+						String date1 = dates[i];
+						String date2 = dates[i+1];
+						pstmt2.clearParameters();
+						pstmt2.setInt(1, home);
+						pstmt2.setString(2, date1);
+						pstmt2.setString(3, date2);
+						ResultSet rs = pstmt2.executeQuery();
+						if(rs.next()){
+							pstmt.clearParameters();
+							pstmt.setInt(1,home);
+							pstmt.setString(2,date1);
+							pstmt.setString(3, date2);
+							pstmt.executeUpdate();
+							System.out.println(date1 + "-" + date2 + " removed");
+						}
+							
+						else{
+							System.out.println(date1 + "-" + date2 + " does not exist so it was ignored");
+						}
+						rs.close();
+					}
+					
+					conn.closeConnection();
+
 
 				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
 				
-				rs.close();
-				conn.closeConnection();
+				break;
+			case 3:
+				ShowDates(home);
+				break;
+			case 4:
+
+				
+				try{
+					Connector conn = new Connector();
+					Set<String> wordSet = GetKeyWords(home);
+					System.out.print("\nPlease enter in new home keywords with a space between each new keyword:  ");
+					String[] words = input.nextLine().split("[ ]+");
+					String sql = "INSERT INTO Keywords VALUES(?,?)";
+					PreparedStatement pstmt = conn.con.prepareStatement(sql);
+
+					for(int i = 0; i < words.length; i++){
+						String name = words[i];
+						if(wordSet.contains(name))
+							System.out.println(name + " already exists so it was ignored");
+						else{
+							pstmt.clearParameters();
+							pstmt.setInt(1,home);
+							pstmt.setString(2,name);
+							pstmt.executeUpdate();
+							System.out.println(name + " added");
+						}
+
+					}
+					
+					conn.closeConnection();
 
 
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+				
+				break;
+			case 5:
+				try{
+					Connector conn = new Connector();
+					Set<String> wordSet = GetKeyWords(home);
+					System.out.print("\nPlease enter in keywords to delete with a space between each new keyword:  ");
+					String[] words = input.nextLine().split("[ ]+");
+					String sql = "delete from Keywords where Keywords.house_id = ? and Keywords.keyword = ?;";
+					PreparedStatement pstmt = conn.con.prepareStatement(sql);
+
+					for(int i = 0; i < words.length; i++){
+						String name = words[i];
+						if(wordSet.contains(name)){
+							pstmt.clearParameters();
+							pstmt.setInt(1,home);
+							pstmt.setString(2,name);
+							pstmt.executeUpdate();
+							System.out.println(name + " removed");
+						}
+							
+						else{
+							System.out.println(name + " does not exist so it was ignored");
+						}
+
+					}
+					
+					conn.closeConnection();
+
+
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+				
+				break;
+			case 6:
+				GetKeyWords(home);
+				break;
+			case 7:
+				try{
+					Connector conn = new Connector();
+					String sql = "select username, DATE_FORMAT(from_date,'%m/%d/%Y'),date_format(until_date,'%m/%d/%Y'), cost, party_count"
+							+ " from Reservations"
+							+ " where house_id = ?";
+					PreparedStatement pstmt = conn.con.prepareStatement(sql);
+					pstmt.clearParameters();
+					pstmt.setInt(1, home);
+					ResultSet rs = pstmt.executeQuery();
+					System.out.println("\n---Current Reservations---");
+					while(rs.next()){
+						System.out.print("username:"+ rs.getString(1));
+						System.out.print(" from:"+ rs.getString(2));
+						System.out.print(" until:"+ rs.getString(3));
+						System.out.print(" cost:"+ rs.getString(4));
+						System.out.println(" party count:"+ rs.getString(5));
+						
+					}
+					rs.close();
+					conn.closeConnection();
+					
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+				break;
+			case 8:
+				display.page = new NewTHPage();
+				go = false;
+				break;
+			case 9:
+				go = false;
+				display.go = false;
 			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-			
-			System.out.println("\nWould you like to update another home?");
-			break;
-		case 3:
-			display.page = new NewTHPage();
-			break;
-		case 4:
-			display.go = false;
 		}
 		
 		
+		
+
+	}
+	
+	private Set<String> GetKeyWords(int home){
+		Set<String> words = new HashSet<String>();
+		try{
+			Connector conn = new Connector();
+			System.out.println("\n---Current Key Words---");
+			ResultSet rs=conn.stmt.executeQuery("Select keyword from Keywords where Keywords.house_id = "+ Integer.toString(home)+";");
+			while(rs.next()){
+				String word = rs.getString(1);
+				words.add(word);
+				System.out.println(word);
+			}
+			conn.closeConnection();
+			rs.close();
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return words;
+	}
+	
+	private void ShowDates(int home){
+		
+		try{
+			String sql = "select d.from_date, d.until_date"
+					+ " from (select from_date, until_date"
+					+ 		" from Dates_Available"
+					+ 		" where house_id = ?) as d";
+			Connector conn = new Connector();
+			PreparedStatement pstmt = conn.con.prepareStatement(sql);
+			pstmt.setInt(1, home);
+			ResultSet rs = pstmt.executeQuery();
+			System.out.println("\n---Current Dates---");
+			while(rs.next()){
+				System.out.println(format.format(rs.getDate(1)) + "-" + format.format(rs.getDate(2)));
+			}
+			conn.closeConnection();
+			rs.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 
 	}
 }
